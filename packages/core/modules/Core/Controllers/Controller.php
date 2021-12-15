d22fbb18886a1ef698a5f5f7278785c1ec075fda
Ondřej Langr <hi@ondrejlangr.cz>
2021-12-15 16:55:30 +0100

    feature(Core): Created first real face of core. The PHP barebone app.
    After everything - everything will be tied together after export.

---
+++ packages/core/modules/Core/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Controller
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $response;
+    }
+}
