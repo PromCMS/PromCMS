48d86fdc68205dc2e4caaa6c27ed82d4aeff3324
Ondřej Langr <hi@ondrejlangr.cz>
2022-04-12 20:33:36 +0200

    New generator constant

--- packages/shared/src/generator-constants.ts
+++ packages/shared/src/generator-constants.ts
@@ -10,4 +10,5 @@ export const ADMIN_PROJECT_ROOT = join(PROJECT_ROOT, 'packages', 'admin');
 export const CORE_ROOT = join(PROJECT_ROOT, 'packages', 'core');
 
 export const GENERATOR_FILENAME = 'prom.generate-config.js';
+export const GENERATOR_FILENAME__JSON = 'prom.generate-config.json';
 export const GENERATOR_FILE_PATH = join(PROJECT_ROOT, GENERATOR_FILENAME);
