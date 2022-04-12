ff66b6493635622767e85e5ace71110be0b77556
Ondřej Langr <hi@ondrejlangr.cz>
2022-04-12 07:58:13 +0200

    Accept custom cwd for env loading

--- packages/shared/src/utils/env.ts
+++ packages/shared/src/utils/env.ts
@@ -1,12 +1,12 @@
-export const getEnvFilepath = async () => {
+export const getEnvFilepath = async (anotherRoot?: string) => {
   const findConfig = (await import(/* webpackIgnore: true */ 'find-config'))
     .default;
 
-  return findConfig('.env') || '.env';
+  return findConfig('.env', { cwd: anotherRoot }) || '.env';
 };
 
-export const loadRootEnv = async () => {
+export const loadRootEnv = async (anotherRoot?: string) => {
   const dotenv = (await import(/* webpackIgnore: true */ 'dotenv')).default;
 
-  dotenv.config({ path: await getEnvFilepath() });
+  dotenv.config({ path: await getEnvFilepath(anotherRoot) });
 };
