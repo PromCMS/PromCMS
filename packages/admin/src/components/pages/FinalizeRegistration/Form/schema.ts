224af0659ba0d90f242b6580759f665e7fcc173f
Ondřej Langr <hi@ondrejlangr.cz>
2022-04-26 18:07:53 +0200

    Finalize registration page

---
+++ packages/admin/src/components/pages/FinalizeRegistration/Form/schema.ts
@@ -0,0 +1,11 @@
+import { object, string, ref } from 'yup'
+
+export const finalizeRegistrationFormSchema = object()
+  .shape({
+    new_password: string().required('Dont forget to include this value'),
+    confirmed_new_password: string()
+      .required('Dont forget to include this value')
+      .oneOf([ref('new_password'), null], 'Passwords must match'),
+    token: string().required('Dont forget to include this value'),
+  })
+  .noUnknown()
