6d4c344fc2b4c4d0925aa2508b087eada8f4c106
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-05 16:52:08 +0200

    Change php deps tree

--- packages/cli/src/parts/install-php-deps.ts
+++ packages/cli/src/parts/install-php-deps.ts
@@ -3,11 +3,10 @@ import { execa } from 'execa';
 export const installPHPDeps = async (appRoot: string) => {
   const depsList = [
     'slim/slim:4.*',
-    'illuminate/pagination:8.77.1',
     'guzzlehttp/psr7:^2',
     'guzzlehttp/guzzle:^7.4',
     'php-di/php-di:^6.3',
-    'illuminate/database:^8.77',
+    'rakibtg/sleekdb',
     'bryanjhv/slim-session:^4.0',
     'league/flysystem:2.1.1',
     'firebase/php-jwt:^6.0',
@@ -15,7 +14,7 @@ export const installPHPDeps = async (appRoot: string) => {
     'phpmailer/phpmailer:^6.6',
     'twig/twig:^3.0',
     'symfony/dotenv:^5.4',
-    'illuminate/events:^8.83',
+    'illuminate/support',
   ];
 
   const devDepsList = ['fakerphp/faker:^1.9.1', 'mockery/mockery:^1.3.1'];
