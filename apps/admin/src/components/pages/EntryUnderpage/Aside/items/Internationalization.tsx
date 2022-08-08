450a01ae62451a1c7ddf0c2242d89164c23e60f5
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-08 21:48:45 +0200

    Use new LanguageSelect component

--- apps/admin/src/components/pages/EntryUnderpage/Aside/items/Internationalization.tsx
+++ apps/admin/src/components/pages/EntryUnderpage/Aside/items/Internationalization.tsx
@@ -1,27 +1,22 @@
 import AsideItemWrap from '@components/AsideItemWrap';
-import { useSettings } from '@hooks/useSettings';
-import { Select } from '@mantine/core';
+import { LanguageSelect } from '@components/form/LanguageSelect';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useEntryUnderpageContext } from '../../context';
 
 export const Internationalization: FC = () => {
   const { t } = useTranslation();
-  const settings = useSettings();
   const { currentView, language, setLanguage } = useEntryUnderpageContext();
 
   return (
     <AsideItemWrap title={t('Internationalization')}>
       <div className="mx-5 mb-5">
-        <Select
-          data={settings?.i18n?.languages || []}
-          label={t('Language')}
+        <LanguageSelect
           value={language}
           onChange={(value) => value && setLanguage(value)}
           className="w-full"
-          placeholder={t('Select an option')}
           shadow="xl"
-          disabled={!settings || currentView === 'create'}
+          disabled={currentView === 'create'}
         />
       </div>
     </AsideItemWrap>
