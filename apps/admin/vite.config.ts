607891b8d53b45987af6a427b54758d1e2529ab9
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-10 20:48:18 +0200

    Update vite config

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,18 +1,36 @@
-import { defineConfig } from 'vite';
+import * as path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-const { PORT = 3004 } = process.env;
-const isDev = process.env.NODE_ENV == 'development';
-const APP_PORT = Number(PORT);
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, path.join(process.cwd(), '..', '..'), '');
+  const { PORT = 3004 } = env;
+  const isDev = mode == 'development';
+  const APP_PORT = Number(PORT);
+  const APP_URL_PREFIX = isDev ? undefined : '/admin/';
 
-export default defineConfig({
-  base: isDev ? '/' : '/admin',
-  server: {
-    port: Number(APP_PORT),
-    proxy: {
-      '^/api/.*': {
-        target: `http://localhost:${APP_PORT + 1}`,
-        changeOrigin: false,
+  return {
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      visualizer({
+        filename: './dev/stats.html',
+      }),
+    ],
+    base: APP_URL_PREFIX || '/',
+    define: {
+      APP_URL_PREFIX,
+    },
+    server: {
+      port: Number(APP_PORT),
+      proxy: {
+        '^/api/.*': {
+          target: `http://localhost:${APP_PORT + 1}`,
+          changeOrigin: false,
+        },
       },
     },
-  },
+  };
 });
