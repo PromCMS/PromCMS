569b2e1fce1cb6c069e18fcc6039bf36c56f3d47
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-19 09:59:22 +0200

    Remove unnecessary php deps. They will be installed through prom-cms/core

--- packages/cli/src/parts/install-php-deps.ts
+++ packages/cli/src/parts/install-php-deps.ts
@@ -1,24 +1,9 @@
 import { execa } from 'execa';
 
 export const installPHPDeps = async (appRoot: string) => {
-  const depsList = [
-    'slim/slim:4.*',
-    'guzzlehttp/psr7:^2',
-    'guzzlehttp/guzzle:^7.4',
-    'php-di/php-di:^6.3',
-    'rakibtg/sleekdb',
-    'bryanjhv/slim-session:^4.0',
-    'league/flysystem:2.1.1',
-    'firebase/php-jwt:^6.0',
-    'paragonie/sodium_compat:^1.17',
-    'phpmailer/phpmailer:^6.6',
-    'twig/twig:^3.0',
-    'symfony/dotenv:^5.4',
-    'prom-cms/core',
-    'illuminate/support',
-  ];
+  const depsList = ['prom-cms/core'];
 
-  const devDepsList = ['fakerphp/faker:^1.9.1', 'mockery/mockery:^1.3.1'];
+  const devDepsList = ['fakerphp/faker:^1.9.1'];
 
   await execa('composer', ['require', ...depsList], {
     cwd: appRoot,
