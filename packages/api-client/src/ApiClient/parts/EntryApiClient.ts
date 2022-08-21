532e999971f9b5d17c70d9bdc7e6eba811cc1c0e
Ondřej Langr <hi@ondrejlangr.cz>
2022-08-21 11:13:35 +0200

    Format

--- packages/api-client/src/ApiClient/parts/EntryApiClient.ts
+++ packages/api-client/src/ApiClient/parts/EntryApiClient.ts
@@ -1,6 +1,6 @@
-import { ItemID } from "@prom-cms/shared";
-import { PagedResponse, QueryParams, Response, ResultItem } from "../../types";
-import { formatQueryParams } from "../../utils";
+import { ItemID } from '@prom-cms/shared';
+import { PagedResponse, QueryParams, Response, ResultItem } from '../../types';
+import { formatQueryParams } from '../../utils';
 import { ApiClientBase } from '../ApiClientBase';
 
 export class EntryApiClient extends ApiClientBase {
@@ -21,24 +21,42 @@ export class EntryApiClient extends ApiClientBase {
   }
 
   async getMany<T extends ResultItem>(modelId: string, options: QueryParams) {
-    return this.axios.get<PagedResponse<T>>(EntryApiClient.getItemsUrl(modelId), {
-      params: formatQueryParams(options),
-    });
+    return this.axios.get<PagedResponse<T>>(
+      EntryApiClient.getItemsUrl(modelId),
+      {
+        params: formatQueryParams(options),
+      }
+    );
   }
 
-  async update<T extends ResultItem>(modelId: string, id: ItemID, payload: Omit<ResultItem, "id">) {
-    return this.axios.patch<Response<T>>(EntryApiClient.getItemUrl(modelId, id), {
-      data: payload,
-    });
+  async update<T extends ResultItem>(
+    modelId: string,
+    id: ItemID,
+    payload: Omit<ResultItem, 'id'>
+  ) {
+    return this.axios.patch<Response<T>>(
+      EntryApiClient.getItemUrl(modelId, id),
+      {
+        data: payload,
+      }
+    );
   }
 
   async delete<T extends ResultItem>(modelId: string, id: ItemID) {
-    return this.axios.delete<Response<T>>(EntryApiClient.getItemUrl(modelId, id));
+    return this.axios.delete<Response<T>>(
+      EntryApiClient.getItemUrl(modelId, id)
+    );
   }
 
-  async create<T extends ResultItem>(modelId: string, payload: Omit<ResultItem, "id">) {
-    return this.axios.post<Response<T>>(`${EntryApiClient.getItemsUrl(modelId)}/create`, {
-      data: payload,
-    });
+  async create<T extends ResultItem>(
+    modelId: string,
+    payload: Omit<ResultItem, 'id'>
+  ) {
+    return this.axios.post<Response<T>>(
+      `${EntryApiClient.getItemsUrl(modelId)}/create`,
+      {
+        data: payload,
+      }
+    );
   }
 }
--- packages/api-client/src/ApiClient/parts/FileApiClient.ts
+++ packages/api-client/src/ApiClient/parts/FileApiClient.ts
@@ -1,6 +1,12 @@
-import { File as FileItem, ItemID } from '@prom-cms/shared';
-import { QueryParams, Response, FileInput, PagedResponse } from "../../types";
-import { formatQueryParams } from "../../utils";
+import { ItemID } from '@prom-cms/shared';
+import {
+  QueryParams,
+  Response,
+  FileInput,
+  PagedResponse,
+  FileItem,
+} from '../../types';
+import { formatQueryParams } from '../../utils';
 import { ApiClientBase } from '../ApiClientBase';
 import { EntryApiClient } from './EntryApiClient';
 
@@ -9,31 +15,42 @@ export class FileApiClient extends ApiClientBase {
 
   getFileUrl(fileId: string, searchParams: Record<string, string> = {}) {
     const params = new URLSearchParams(searchParams).toString();
-    return `${EntryApiClient.getItemUrl(this.modelId, fileId)}/raw${params ? `?${params}` : ''
-      }`;
+    return `${EntryApiClient.getItemUrl(this.modelId, fileId)}/raw${
+      params ? `?${params}` : ''
+    }`;
   }
 
   async getOne(id: ItemID) {
-    return this.axios.get<Response<FileItem>>(EntryApiClient.getItemUrl(this.modelId, id));
+    return this.axios.get<Response<FileItem>>(
+      EntryApiClient.getItemUrl(this.modelId, id)
+    );
   }
 
   async getMany(options: QueryParams = {}) {
-    return this.axios.get<PagedResponse<FileItem>>(EntryApiClient.getItemsUrl(this.modelId), {
-      params: formatQueryParams(options),
-    });
+    return this.axios.get<PagedResponse<FileItem>>(
+      EntryApiClient.getItemsUrl(this.modelId),
+      {
+        params: formatQueryParams(options),
+      }
+    );
   }
 
   async update(id: ItemID, payload: FileInput) {
-    return this.axios.patch<Response<FileItem>>(EntryApiClient.getItemUrl(this.modelId, id), {
-      data: payload,
-    });
+    return this.axios.patch<Response<FileItem>>(
+      EntryApiClient.getItemUrl(this.modelId, id),
+      {
+        data: payload,
+      }
+    );
   }
 
   async delete(id: ItemID) {
-    return this.axios.delete<Response<FileItem>>(EntryApiClient.getItemUrl(this.modelId, id));
+    return this.axios.delete<Response<FileItem>>(
+      EntryApiClient.getItemUrl(this.modelId, id)
+    );
   }
 
-  async create(file: File, info?: Omit<FileItem, "id">) {
+  async create(file: File, info?: Omit<FileItem, 'id'>) {
     const formData = new FormData();
     formData.append('file', file);
 
