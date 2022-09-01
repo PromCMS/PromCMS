aa1154502d5872dfabf5d17da18b0617116526f5
Ondřej Langr <hi@ondrejlangr.cz>
2022-09-01 17:14:58 +0200

    Remove admin services

--- apps/admin/src/misc/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './EntryService';
-export * from './UserService';
-export * from './ProfileService';
-export * from './FileService';
-export * from './SettingsService';
-export * from './UserRolesService';
-export * from './FolderService';
