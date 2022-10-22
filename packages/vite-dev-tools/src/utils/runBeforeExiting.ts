43da6747e3e48f693c4312d889bfaa1f82317e12
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-22 23:49:07 +0200

    Add utils

---
+++ packages/vite-dev-tools/src/utils/runBeforeExiting.ts
@@ -0,0 +1,24 @@
+import { ExitSignals } from '../types';
+
+export const runBeforeExiting = (fun: Function) => {
+  let wasCleanedUp = false;
+  const exitSignals: ExitSignals[] = [
+    'exit',
+    'SIGINT',
+    'SIGUSR1',
+    'SIGUSR2',
+    'uncaughtException',
+  ];
+
+  for (const signal of exitSignals) {
+    process.on(signal, async () => {
+      // eslint-disable-line @typescript-eslint/no-explicit-any
+      if (!wasCleanedUp) {
+        await fun();
+        wasCleanedUp = true;
+      }
+
+      process.exit();
+    });
+  }
+};
