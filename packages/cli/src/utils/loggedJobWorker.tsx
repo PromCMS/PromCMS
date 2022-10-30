c56b54cd5261fb182e5860b5bb8061a34fba8d81
Ondřej Langr <hi@ondrejlangr.cz>
2022-10-30 21:08:58 +0100

    Update loggedJobWorker

--- packages/cli/src/utils/loggedJobWorker.tsx
+++ packages/cli/src/utils/loggedJobWorker.tsx
@@ -1,102 +1,57 @@
 import { ArgList, Command, GlobalOptions } from '@boost/cli';
+import type { PromptProps } from '@boost/cli/src/components/internal/Prompt';
 import { useProgram } from '@boost/cli/react';
-import { Text, Box, useInput } from 'ink';
-import { FC, useEffect, useState } from 'react';
+import { Spinner, WorkerJob } from '../components';
+import { Text, Box } from 'ink';
+import { FC, useCallback, useState } from 'react';
+import { LoggedWorkerJob, MaybePromise } from 'types';
 
-const LAST_JOB_TITLE = '__last';
-
-export type LoggedWorkerJob = {
-  title: string;
-  job: () => Promise<void>;
-  skip?: boolean;
+export type Prompts = { items: PromptItem[] };
+export type PromptItem = {
+  type: (props: any) => JSX.Element;
+  props?: Omit<PromptProps<any>, 'onSubmit'> & Record<string, any>;
 };
 
-const symbols = ['|', '/', '-', '\\'] as const;
-type ActiveSymbols = typeof symbols[number];
+export const getWorkerJob = <T extends Record<string, unknown>>(
+  title: string,
+  options: Omit<LoggedWorkerJob, 'job' | 'prompts' | 'title'> & {
+    job: (context?: T) => MaybePromise<void>;
+    prompts?: () => MaybePromise<Record<keyof T, PromptItem>>;
+  }
+) => ({
+  title,
+  ...options,
+});
 
-const Spinner: FC<{
-  /**
-   * @defaultValue false
-   */
-  disabled?: boolean;
-}> = ({ disabled = false }) => {
-  const [activeSymbol, setActiveSymbol] = useState<ActiveSymbols>('|');
+const Component: FC<{
+  jobs: LoggedWorkerJob<any>[];
+}> = ({ jobs }) => {
+  const [shownJobs, setShownJobs] = useState(0);
+  const isDone = shownJobs === jobs.length;
+  const { exit: exitProgram } = useProgram();
 
-  useEffect(() => {
-    let interval: NodeJS.Timer;
-
-    if (!disabled) {
-      interval = setInterval(() => {
-        setActiveSymbol((prev) => {
-          const prevIndex = symbols.findIndex((symb) => symb === prev);
-
-          if (prevIndex === symbols.length - 1) {
-            return symbols[0];
-          }
-
-          return symbols[prevIndex + 1];
-        });
-      }, 150);
-    }
-
-    return () => clearInterval(interval);
-  }, [disabled]);
-
-  return <>{activeSymbol}</>;
-};
-
-const Component: FC<{ jobs: LoggedWorkerJob[] }> = ({ jobs }) => {
-  const [activeJobs, setActiveJobs] = useState<LoggedWorkerJob[]>([]);
-  const { exit: exitProgram, log } = useProgram();
-
-  useEffect(() => {
-    const run = async () => {
-      try {
-        for (const jobInfo of jobs) {
-          const { job, skip } = jobInfo;
-          setActiveJobs((prev) => [jobInfo, ...prev]);
-
-          if (skip) {
-            continue;
-          }
-          await job();
-        }
-
-        setActiveJobs((prev) => [
-          { job: async () => {}, title: LAST_JOB_TITLE, skip: true },
-          ...prev,
-        ]);
-      } catch (error) {
-        exitProgram(error as Error);
+  const onJobDone = useCallback(() => {
+    setShownJobs((prev) => {
+      const nextValue = prev + 1;
+      if (nextValue === jobs.length) {
+        setTimeout(() => {
+          exitProgram();
+        }, 200);
       }
-    };
 
-    run();
-  }, [jobs]);
-
-  const prevJobs = activeJobs.slice(1, 3);
-  const latestElement: LoggedWorkerJob | undefined = activeJobs[0];
-  const isDone = latestElement?.title === LAST_JOB_TITLE;
-
-  useInput(() => {
-    if (isDone) {
-      exitProgram();
-    }
-  });
+      return nextValue;
+    });
+  }, []);
 
   return (
     <Box flexDirection="column">
-      {prevJobs.reverse().map(({ title, skip }, index) => (
-        <Text key={title} color="grey">
-          {skip ? 'SKIPPED' : 'DONE'} {title}
-        </Text>
+      {jobs.slice(0, shownJobs + 1).map((props) => (
+        <WorkerJob key={props.title} info={props} onSuccess={onJobDone} />
       ))}
-      {latestElement && !isDone && (
-        <Text color="blue">WORKING {latestElement.title}</Text>
-      )}
+
       {isDone ? (
         <Text color="white" backgroundColor="white">
-          Done! Press any key to exit..
+          Done! Bye!
         </Text>
       ) : (
         <Text color="green">
@@ -108,7 +63,8 @@ const Component: FC<{ jobs: LoggedWorkerJob[] }> = ({ jobs }) => {
 };
 
 export async function loggedJobWorker<
-  T extends Command<GlobalOptions, ArgList, {}>
->(this: T, jobs: LoggedWorkerJob[]) {
+  T extends Command<GlobalOptions, ArgList, {}>,
+  J extends LoggedWorkerJob<any>[]
+>(this: T, jobs: J) {
   await this.render(<Component jobs={jobs} />);
 }
