3c6676386abc2025b93a83073371a9e79fa0803e
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-04 15:31:29 +0100

    Update generate-cms tests

--- packages/cli/tests/commands/generate-cms.test.ts
+++ packages/cli/tests/commands/generate-cms.test.ts
@@ -1,38 +1,42 @@
-import { afterAll, beforeEach, describe, it } from 'vitest';
-import { execa, Options } from 'execa';
+import { describe, it } from 'vitest';
+/*
+import { execa } from 'execa';
 import path from 'path';
 import fs from 'fs-extra';
 import { mockedGeneratorConfig, PROJECT_ROOT } from '../../src/constants';
 import { GENERATOR_FILENAME__JSON } from '@prom-cms/shared';
 
-const runNpmCommand = (args: string[], options?: Options) =>
-  execa('npm', args, options);
-
 const TEST_FOLDER_PATH = path.join(PROJECT_ROOT, 'node_modules', '.test-data');
-
 describe('commands', () => {
   describe('generate-cms', () => {
-    beforeEach(() => {
-      fs.ensureDirSync(TEST_FOLDER_PATH);
-      fs.emptyDirSync(TEST_FOLDER_PATH);
+    beforeEach(async () => {
+      await fs.ensureDir(TEST_FOLDER_PATH);
+      await fs.emptyDir(TEST_FOLDER_PATH);
     });
 
-    afterAll(() => {
-      fs.removeSync(TEST_FOLDER_PATH);
+    afterAll(async () => {
+      await fs.remove(TEST_FOLDER_PATH);
     });
 
     it('should run correctly and generate files', async () => {
       // create config file
       await fs.writeFile(
         path.join(TEST_FOLDER_PATH, GENERATOR_FILENAME__JSON),
-        mockedGeneratorConfig
+        JSON.stringify(mockedGeneratorConfig)
       );
 
-      const c = runNpmCommand([`c=${TEST_FOLDER_PATH}`], {
+      const subprocess = execa('npx', ['@prom-cms/cli', 'generate-cms'], {
         cwd: TEST_FOLDER_PATH,
       });
 
-      console.log({ c });
+      await subprocess;
+
+      console.log({ subprocess });
     });
   });
+});
+*/
+
+describe('commands generate-cms', () => {
+  it.todo('Add tests');
 });
