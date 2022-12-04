1be805315dec956015d960343f4eb71f26e8debc
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-04 13:08:38 +0100

    Add react shim for cli project

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,1 +1,9 @@
-export { default } from '@prom-cms/config/default.tsup.mjs';
+import defaultConfig from '@prom-cms/config/default.tsup.mjs';
+import { Options } from 'tsup';
+
+const config: Options = {
+  ...defaultConfig,
+  inject: ['./react-shim.js'],
+};
+
+export default config;
