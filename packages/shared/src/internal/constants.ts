efb8f7a9c4bfd5db0c7007e617c195362c579dc9
Ondřej Langr <hi@ondrejlangr.cz>
2023-01-29 12:49:42 +0100

    Add more shared constants

--- packages/shared/src/internal/constants.ts
+++ packages/shared/src/internal/constants.ts
@@ -1,1 +1,14 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const PORT = 3000;
+
+export const monorepoRoot = path.join(__dirname, '..', '..', '..', '..');
+export const developmentPHPAppPath = path.join(
+  monorepoRoot,
+  'node_modules',
+  '.prom-cms',
+  'php-app'
+);
