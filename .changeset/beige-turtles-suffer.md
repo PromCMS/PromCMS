2c9fe371610ae896499b4f87eef6dc94e3c80cc9
Ondřej Langr <hi@ondrejlangr.cz>
2023-04-13 21:16:37 +0200

    fix: Fix missing php dependency.

---
+++ .changeset/beige-turtles-suffer.md
@@ -0,0 +1,5 @@
+---
+"@prom-cms/cli": patch
+---
+
+Fix missing psr/log dependency to still support 7.4 since we are are not yet ready for version 8
--- packages/cli/src/parts/install-php-deps.ts
+++ packages/cli/src/parts/install-php-deps.ts
@@ -2,7 +2,7 @@ import { execa } from 'execa';
 
 export const installPHPDeps = async (appRoot: string) => {
   const depsList = ['prom-cms/core', 'rakit/validation'];
-  const devDepsList = ['fakerphp/faker:^1.9.1'];
+  const devDepsList = ['fakerphp/faker:^1.9.1', 'psr/log:^1'];
 
   await execa('composer', ['require', ...depsList], {
     cwd: appRoot,
