e6beebd87f8eabf948c6ce3063a53a1a282ea799
Ondřej Langr <hi@ondrejlangr.cz>
2023-12-23 18:39:14 +0100

    Fixes bundle analyzer

--- packages/admin/vite/config.ts
+++ packages/admin/vite/config.ts
@@ -3,7 +3,7 @@ import { execa } from 'execa';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
-import { PluginOption, defineConfig, loadEnv } from 'vite';
+import { Plugin, PluginOption, defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { promPlugin } from '@prom-cms/vite';
@@ -11,6 +11,14 @@ import { promPlugin } from '@prom-cms/vite';
 export default defineConfig(async ({ mode, command }) => {
   const currentFolder = process.cwd();
   const repoRoot = path.join(currentFolder, '..', '..');
+  const projectRoot = path.join(currentFolder, 'src');
+  const developmentProjectPath = path.join(
+    repoRoot,
+    'node_modules',
+    '.prom-cms',
+    'php-app'
+  );
+  let abortController: AbortController | undefined;
 
   const env = loadEnv(mode, repoRoot, '');
   const { PORT = 3000, ANALYZE = false } = env;
@@ -22,14 +30,6 @@ export default defineConfig(async ({ mode, command }) => {
   ];
 
   if (command === 'serve') {
-    let abortController: AbortController | undefined;
-    const developmentProjectPath = path.join(
-      repoRoot,
-      'node_modules',
-      '.prom-cms',
-      'php-app'
-    );
-
     if ((await fs.pathExists(developmentProjectPath)) === false) {
       console.log(
         `PromCMS testing project not present at ${developmentProjectPath}, creating it...`
@@ -41,56 +41,58 @@ export default defineConfig(async ({ mode, command }) => {
 
       console.log('Project created! Happy coding!');
     }
-
-    plugins.push(
-      promPlugin({
-        paths: {
-          phpFiles: developmentProjectPath,
-          project: currentFolder,
-        },
-        onExit() {
-          abortController?.abort();
-        },
-        watchFiles: {
-          files: [],
-          async onChange({ logger, fileInfo }) {
-            // Abort previous
-            if (abortController) {
-              abortController.abort();
-              logger?.warn(
-                `File "${fileInfo.filePath}" has been changed - starting from the start 😪`
-              );
-            } else {
-              logger?.info(`Rebuilding models...`);
-            }
-
-            try {
-              abortController = new AbortController();
-
-              await execa('npm', ['run', 'project:update'], {
-                cwd: path.join(process.cwd(), '../../packages/cli'),
-                signal: abortController.signal,
-              });
-
-              logger?.info(`Model rebuild completed!`);
-            } catch (error) {
-              if (error instanceof Error) {
-                logger?.error(`Model rebuild failed - ${error.message}`);
-              }
-            }
-          },
-        },
-      }) as unknown as PluginOption
-    );
   }
 
   if (ANALYZE) {
     plugins.push(
       visualizer({
         filename: './dev/stats.html',
-      }) as undefined as PluginOption
+        title: 'PromCMS Admin Visualizer',
+        projectRoot,
+      }) as unknown as Plugin
     );
   }
+
+  plugins.push(
+    promPlugin({
+      paths: {
+        phpFiles: developmentProjectPath,
+        project: currentFolder,
+      },
+      onExit() {
+        abortController?.abort();
+      },
+      watchFiles: {
+        files: [],
+        async onChange({ logger, fileInfo }) {
+          // Abort previous
+          if (abortController) {
+            abortController.abort();
+            logger?.warn(
+              `File "${fileInfo.filePath}" has been changed - starting from the start 😪`
+            );
+          } else {
+            logger?.info(`Rebuilding models...`);
+          }
+
+          try {
+            abortController = new AbortController();
+
+            await execa('npm', ['run', 'project:update'], {
+              cwd: path.join(process.cwd(), '../../packages/cli'),
+              signal: abortController.signal,
+            });
+
+            logger?.info(`Model rebuild completed!`);
+          } catch (error) {
+            if (error instanceof Error) {
+              logger?.error(`Model rebuild failed - ${error.message}`);
+            }
+          }
+        },
+      },
+    })
+  );
 
   return {
     clearScreen: false,
@@ -100,12 +102,12 @@ export default defineConfig(async ({ mode, command }) => {
       __APP_URL_PREFIX__: JSON.stringify(APP_URL_PREFIX),
       'process.env': {},
     },
-    root: 'src',
+    root: projectRoot,
     server: {
       port: Number(PORT),
     },
     build: {
-      outDir: '../dist',
+      outDir: path.join(projectRoot, '..', 'dist'),
     },
   };
 });
