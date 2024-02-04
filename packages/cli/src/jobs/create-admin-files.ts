92369edfce1bac1db2415a90c8f518cca1432e23
Ondřej Langr <hi@ondrejlangr.cz>
2024-02-04 09:01:53 +0100

    Combines multiple resolve usages to resolve admin package

--- packages/cli/src/jobs/create-admin-files.ts
+++ packages/cli/src/jobs/create-admin-files.ts
@@ -1,3 +1,4 @@
+import { PACKAGE_ROOT } from '@constants';
 import fs from 'fs-extra';
 import path from 'path';
 import resolve from 'resolve';
@@ -6,9 +7,8 @@ export type CreateAdminFilesOptions = {
   cwd: string;
 };
 
-export const createAdminFiles = async ({ cwd }: CreateAdminFilesOptions) => {
-  // We will just prebuild admin from package on npm
-  const adminRoot = await new Promise<string>((finish, reject) =>
+const resolveAdminRoot = () =>
+  new Promise<string>((finish, reject) =>
     resolve('@prom-cms/admin', (err, resolved) => {
       if (err) {
         return reject(err);
@@ -21,6 +21,25 @@ export const createAdminFiles = async ({ cwd }: CreateAdminFilesOptions) => {
       finish(resolved);
     })
   );
+
+export const createAdminFiles = async ({ cwd }: CreateAdminFilesOptions) => {
+  // Firstly it could be installed in cli package scope
+  let adminRoot = path.resolve(
+    PACKAGE_ROOT,
+    'node_modules',
+    '@prom-cms',
+    'admin'
+  );
+
+  // Falls back to global scope
+  if (!fs.existsSync(adminRoot)) {
+    adminRoot = path.resolve(PACKAGE_ROOT, '..', 'admin');
+  }
+
+  // Falls back to resolve package, if even that fails then it throws
+  if (!fs.existsSync(adminRoot)) {
+    adminRoot = await resolveAdminRoot();
+  }
 
   // And the copy
   const adminFinalPath = path.join(cwd, 'public', 'admin');
