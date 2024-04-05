7178caa89b82944bd765d8208e614cd76dfc14df
Ondřej Langr <hi@ondrejlangr.cz>
2024-04-05 15:31:03 +0900

    Extends WhereQueryParameter possible manipulators

---
+++ .changeset/lucky-carpets-agree.md
@@ -0,0 +1,5 @@
+---
+"@prom-cms/api-client": minor
+---
+
+Extents possible manipulator for where query param on requests.
--- packages/api-client/src/types/WhereQueryParameter.ts
+++ packages/api-client/src/types/WhereQueryParameter.ts
@@ -2,7 +2,15 @@ export type WhereQueryParameter = Record<
   string,
   | {
       value: string | number;
-      manipulator: 'LIKE' | '>' | '<' | '=' | string;
+      manipulator:
+        | 'LIKE'
+        | 'NOTLIKE'
+        | 'IN'
+        | 'NOT IN'
+        | '>'
+        | '<'
+        | '='
+        | string;
     }
   | {
       value: (string | number)[];
